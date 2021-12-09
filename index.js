f8518e9bffa462138d10dacf70559d74ee9b5898
Tanja <tanja120a@gmail.com>
2021-12-09 10:13:47 +0100

    Set static folder

--- index.js
+++ index.js
@@ -2,11 +2,6 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-
-/*Static server - Create server for static pages
-    app.get('/', (req, res) => {
-        res.sendFile(path.join(__dirname, 'public', 'index.html'));
-    })*/
     
 //Set Static folder   
 app.use(express.static(path.join(__dirname, 'public')))
